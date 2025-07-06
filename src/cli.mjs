7beae788a74149a97cf46f2b888c89a98e20d5d8
Attila Gonda <pcdevil@users.noreply.github.com>
2025-07-06 11:05:37 +0100

    feat: update dependencies

--- src/cli.mjs
+++ src/cli.mjs
@@ -1,6 +1,11 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
 // project imports
 import { WordOfTheDay } from '#src/word-of-the-day.mjs';
+
+dotenv.config({
+	processEnv: process.env,
+	quiet: true,
+});
 
 const wordOfTheDay = new WordOfTheDay();
 await wordOfTheDay.run();
